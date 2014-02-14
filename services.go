5c439ff36ec3f7e8ab1c7b59a9e8b21c0d17b017
Siddhartha Basu <biosidd@gmail.com>
2014-02-14 03:44:47 -0500

    added printing of attribute in service command

--- services.go
+++ services.go
@@ -1,39 +1,53 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"log"
-
-	"github.com/flynn/go-discoverd"
+    "flag"
+    "fmt"
+    discoverd "github.com/flynn/go-discoverd"
+    "log"
 )
 
 type services struct {
-	clientCmd
-	onlyOne *bool
+    clientCmd
+    onlyOne   *bool
+    printAttr *bool
 }
 
 func (cmd *services) Name() string {
-	return "services"
+    return "services"
 }
 
 func (cmd *services) DefineFlags(fs *flag.FlagSet) {
-	cmd.onlyOne = fs.Bool("1", false, "only show one service")
+    cmd.onlyOne = fs.Bool("1", false, "only show one service")
+    cmd.printAttr = fs.Bool("a", false, "output attributes if any")
 }
 
 func (cmd *services) Run(fs *flag.FlagSet) {
-	cmd.InitClient(false)
-	services, err := cmd.client.Services(fs.Arg(0), discoverd.DefaultTimeout)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if *cmd.onlyOne {
-		if len(services) > 0 {
-			fmt.Println(services[0].Addr)
-		}
-		return
-	}
-	for _, service := range services {
-		fmt.Println(service.Addr)
-	}
+    cmd.InitClient(false)
+    services, err := cmd.client.Services(fs.Arg(0), discoverd.DefaultTimeout)
+    if err != nil {
+        log.Fatal(err)
+    }
+    if *cmd.onlyOne {
+        if len(services) > 0 {
+            cmd.PrintService(services[0])
+        }
+        return
+    }
+    for _, service := range services {
+        cmd.PrintService(service)
+    }
+}
+
+func (cmd *services) PrintService(srv *discoverd.Service) {
+    fmt.Printf("%s", srv.Addr)
+    fmt.Printf(";name=%s:host=%s:port=%s", srv.Name, srv.Host, srv.Port)
+    if *cmd.printAttr {
+        fmt.Printf(";")
+        for k, v := range srv.Attrs {
+            fmt.Printf("%s=%s", k, v)
+        }
+    }
+    fmt.Printf("\n")
+
 }
